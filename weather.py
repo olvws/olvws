86a392b8d520acd3f8a340e0b755942312d528aa
Blake Ford <livewire@Macbook.local>
2012-10-22 20:34:06 -0500

    Improve temp response reliability

--- weather.py
+++ weather.py
@@ -9,7 +9,8 @@ def current_temp(parameter):
 	updated_html = wsutils.standard_service_header
 	woeid = parameter[parameter.index("=")+1:]
 	d = feedparser.parse('http://weather.yahooapis.com/forecastrss?w='+woeid)
-	updated_html = updated_html+wsutils.terminal_begin+"temp"+wsutils.terminal_middle+d.entries[0].description.split(" ")[7]+wsutils.terminal_end
+	filtered_desc =  d.entries[0].description[d.entries[0].description.index(',')+1:]
+	updated_html = updated_html+wsutils.terminal_begin+"temp"+wsutils.terminal_middle+filtered_desc[:filtered_desc.index('F')]+wsutils.terminal_end
 	updated_html = updated_html+wsutils.standard_service_footer
 	f = open("./Weather/currentTemp", "w")
 	f.write(updated_html)
