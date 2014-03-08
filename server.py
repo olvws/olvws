9dafb00e60b6158b46056527b634a88ecb6dcddf
Blake W. Ford <blakewford@gmail.com>
2014-03-07 18:17:46 -0600

    Make accessible via javascript, allow script to run directly from command line

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python 
 import cgi, SocketServer, SimpleHTTPServer
 import wsutils, timeserver, weather
  
@@ -12,6 +13,10 @@ class Handler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 			weather.current_temp_default()
                 
         return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+
+    def end_headers(self):
+        self.send_header("Access-Control-Allow-Origin", "*")
+        SimpleHTTPServer.SimpleHTTPRequestHandler.end_headers(self)
 
 wsutils.generate_user_services_file()
 httpd = SocketServer.TCPServer(("", 8080), Handler)
